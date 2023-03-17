671aaf37db916685ab7c48bdf03a28c1c5f19bf6
deanb <deanb93@live.co.uk>
2023-03-17 19:40:08 +0000

    correcting data type of EarthMass

--- PlanetDemoWithApi/Shared/Planet.cs
+++ PlanetDemoWithApi/Shared/Planet.cs
@@ -5,29 +5,34 @@ namespace PlanetDemoWithApi.Shared
     public class Planet
     {
         [Description("Name of the Planet")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
         [Description("URL that will be used to display a photo of the planet")]
-        public string ImageUrl { get; set; }
+        public string? ImageUrl { get; set; }
         [Description("Distance from the Sun in Astronomical Units")]
-        public decimal DistanceFromSun { get; set; }
+        public double? DistanceFromSun { get; set; }
         [Description("Mass of planet in billions of kilograms")]
-        public string ActualMass { get; set; }
+        public string? ActualMass { get; set; }
         [Description("Mass of planet in relation to Earth (M⊕)")]
-        public string EarthMass { get; set; }
+        public double? EarthMass { get; set; }
         [Description("Equatorial diameter of planet in kilometers")]
-        public int Diameter { get; set; }
+        public int? Diameter { get; set; }
         [Description("Average surface temperature of the planet in celsius")]
-        public int SurfaceTemperature { get; set; }
+        public int? SurfaceTemperature { get; set; }
         [Description("Classification of the planet type, e.g. Dwarf Planets, Gas Giant")]
-        public string Classification { get; set; }
+        public string? Classification { get; set; }
         [Description("Length of a year on the planet in Earth Days")]
-        public int LengthOfYear { get; set; }
+        public int? LengthOfYear { get; set; }
         [Description("Number of moons the planet has")]
-        public int NumberOfMoons { get; set; }
+        public int? NumberOfMoons { get; set; }
         [Description("What the planet is named after")]
-        public string Namesake { get; set; }
+        public string? Namesake { get; set; }
 
-        public Planet(string name, string imageUrl, decimal distanceFromSun, string actualMass, string earthMass, int diameter, int surfaceTemperature, string classification, int lengthOfYear, int numberOfMoons, string namesake)
+        public Planet()
+        {
+
+        }
+
+        public Planet(string name, string imageUrl, double distanceFromSun, string actualMass, double earthMass, int diameter, int surfaceTemperature, string classification, int lengthOfYear, int numberOfMoons, string namesake)
         {
             Name = name;
             ImageUrl = imageUrl;
